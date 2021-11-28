f1af55a05dcdc8959a5a77019796fea8d8daacc3
Iswanul Umam <iswanul.umam@gmail.com>
2021-11-28 23:24:52 +0700

    successful implementing test

--- delivery/controllers/todo/todo_test.go
+++ delivery/controllers/todo/todo_test.go
@@ -10,6 +10,7 @@ import (
 	"todo-layered/entities"
 
 	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/stretchr/testify/assert"
 
 	"todo-layered/delivery/controllers/auth"
@@ -38,9 +39,10 @@ func TestGet(t *testing.T) {
 
 func TestCreate(t *testing.T) {
 
-	// var (
-	// 	globalToken = ""
-	// )
+	var (
+		// for other operation
+		globalToken = ""
+	)
 
 	t.Run("TestLogin", func(t *testing.T) {
 		e := echo.New()
@@ -62,37 +64,32 @@ func TestCreate(t *testing.T) {
 		var response auth.LoginResponseFormat
 		json.Unmarshal([]byte(res.Body.String()), &response)
 
-		// globalToken = response.Token
-
-		fmt.Println(">>", response.Token)
-
+		globalToken = response.Token
 		assert.Equal(t, len(response.Token), 159)
 
 	})
 
-	// t.Run("TestCreate", func(t *testing.T) {
-	// 	localToken := globalToken
-	// 	localToken = "ada"
+	t.Run("TestCreate", func(t *testing.T) {
+		localToken := globalToken
+		e := echo.New()
+		requestBody, _ := json.Marshal(map[string]string{
+			"title": "eat lah",
+		})
+		req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(requestBody))
+		res := httptest.NewRecorder()
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", localToken))
+		context := e.NewContext(req, res)
+		context.SetPath("/todo")
 
-	// 	e := echo.New()
-	// 	requestBody, _ := json.Marshal(map[string]string{
-	// 		"title": "eat lah",
-	// 	})
-	// 	req := httptest.NewRequest(http.MethodPost, "/", bytes.NewBuffer(requestBody))
-	// 	res := httptest.NewRecorder()
-	// 	req.Header.Set("Content-Type", "application/json")
-	// 	req.Header.Set(echo.HeaderAuthorization, fmt.Sprintf("Bearer %v", localToken))
-	// 	context := e.NewContext(req, res)
-	// 	context.SetPath("/todo")
+		// setup mocking data
+		todoController := New(mockTodoRepository{})
+		middleware.JWT([]byte("R4HASIA"))(todoController.Create())(context)
 
-	// 	// setup mocking data
-	// 	todoController := New(mockTodoRepository{})
-	// 	middleware.JWT([]byte("R4HASIA"))(todoController.Create())(context)
-
-	// 	var response entities.Todo
-	// 	json.Unmarshal([]byte(res.Body.String()), &response)
-	// 	assert.Equal(t, response.Title, "Eat Blueberry")
-	// })
+		var response entities.Todo
+		json.Unmarshal([]byte(res.Body.String()), &response)
+		assert.Equal(t, response.Title, "Eat Blueberry")
+	})
 }
 
 // =========================== mocking ===========================
